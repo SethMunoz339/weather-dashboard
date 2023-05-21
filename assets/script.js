aa04f6da6884a2452b23f3442db29119dcd83ce7
drteeth <drteeth339@gmail.com>
2023-05-21 10:56:38 -0400

    added current weather function

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,23 @@ document.getElementById('clearButton').addEventListener('click', function() {
     localStorage.clear();
     alert('Search history has been cleared. Refresh the page.');
 });
+
+var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=[apikey]';
+
+    $.ajax({
+      url: currentWeatherURL,
+      method: 'GET'
+    }).then(function(response) {
+      // Display current weather conditions
+      var currentWeather = $('#current-weather');
+      currentWeather.empty();
+
+      var cityName = $('<h2>').text(response.name);
+      var date = dayjs();$('<h4>').text(date.format('MMM D YYYY'));
+      var icon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+      var temperature = $('<p>').text('Temperature: ' + response.main.temp + ' °F');
+      var humidity = $('<p>').text('Humidity: ' + response.main.humidity + '%');
+      var windSpeed = $('<p>').text('Wind Speed: ' + response.wind.speed + ' MPH');
+
+      currentWeather.append(cityName, date, icon, temperature, humidity, windSpeed);
+    });
